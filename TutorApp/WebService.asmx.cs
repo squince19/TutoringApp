736aaed5a0b4eadce186856d8c09381b6564628b
squince <squince@asu.edu>
2019-02-12 20:30:32 -0700

    Implemented User Log on with back end

--- TutorApp/WebService.asmx.cs
+++ TutorApp/WebService.asmx.cs
@@ -45,15 +45,53 @@ namespace TutorApp
             return sqlDt.Rows.Count;
         }
 
-        [WebMethod]
-        public bool LogOn(string uid, string pass)
+        [WebMethod(EnableSession = true)] //NOTICE: gotta enable session on each individual method
+        public bool LogOn(string userName, string userPassword)
         {
+            //we return this flag to tell them if they logged in or not
             bool success = false;
 
+            //our connection string comes from our web.config file like we talked about earlier
             string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["tutorDB"].ConnectionString;
+            //here's our query.  A basic select with nothing fancy.  Note the parameters that begin with @
+            //NOTICE: we added admin to what we pull, so that we can store it along with the id in the session
+            string sqlSelect = "SELECT userName, userPassword FROM users WHERE userName=@idValue and userPassword=@passValue";
 
-            string sqlSelect = "SELECT id FROM a";
+            //set up our connection object to be ready to use our connection string
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            //set up our command object to use our connection, and our query
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            //tell our command to replace the @parameters with real values
+            //we decode them because they came to us via the web so they were encoded
+            //for transmission (funky characters escaped, mostly)
+            sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(userName));
+            sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(userPassword));
+
+            //a data adapter acts like a bridge between our command object and 
+            //the data we are trying to get back and put in a table object
+            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            //here's the table we want to fill with the results from our query
+            DataTable sqlDt = new DataTable();
+            //here we go filling it!
+            sqlDa.Fill(sqlDt);
+            //check to see if any rows were returned.  If they were, it means it's 
+            //a legit account
+            if (sqlDt.Rows.Count > 0)
+            {
+                //if we found an account, store the id and admin status in the session
+                //so we can check those values later on other method calls to see if they 
+                //are 1) logged in at all, and 2) and admin or not
+                
+                //SQ: We might use the following lines later, so I have just commented them out for now!
+                
+                //Session["id"] = sqlDt.Rows[0]["id"];
+                //Session["admin"] = sqlDt.Rows[0]["admin"];
+                success = true;
+            }
+            //return the result!
             return success;
         }
+
     }
 }
